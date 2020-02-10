8232647b98aa550e53dd14ae14747c010282e00f
Connolly James <ConnollyJames@nhsdlions.org>
2020-02-10 12:44:12 -0500

    lexing ignores spaces now

--- lex.py
+++ lex.py
@@ -1,26 +1,54 @@
 def lexate(filename):
     f = open(filename, "r")
-    corn = f.read()
+    stuff = f.read()
     f.close()
+
+    cornparse = []
 
     masterlist = {
         "print": "print",
         "(": "fnop",
         ")": "fnend",
-        "+": "+"
+        "+": "+",
     }
-    
-    cornparse = []
 
-    for ln in corn.split("\n"):
-        if ln[0] == "#":
+    numbers = []
+
+    for i in range(0,9):
+       numbers.append(str(i))
+
+    tokenlist = masterlist.keys()
+
+    for line in stuff.split("\n"):
+        if line[0] == "#":
             continue
+            tbegin = 0
+        tbuffer = ""
         tokens = []
-        for block in ln.split(" "):
-            if len(block) > 1 and block[-1:] == "i":
-                tokens.append(block)
-            else:
-                tokens.append(masterlist[block])
+        i = 0
+        num = False
+        for l in line:
+            for c in tokenlist:
+                if i > len(c) - 1:
+                   break
+                if l in numbers:
+                   tbuffer += str(l)
+                   num = True
+                   break
+                if l == "i" and num:
+                   tbuffer += l
+                   tokens.append(tbuffer)
+                   tbuffer = ""
+                   i = 0
+                   num = False
+                   break
+                if l == c[i]:
+                  tbuffer += l
+                  if tbuffer in tokenlist:
+                     tokens.append(masterlist[tbuffer])
+                     tbuffer = ""
+                     i = 0
+                  else:
+                     i += 1
         cornparse.append(tokens)
-
     return cornparse
