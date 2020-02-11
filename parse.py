c4004bc22dc238f4ece6a397f33f3a8e3951c4a2
urlordjames <urlordjames@gmail.com>
2020-02-11 12:40:00 -0500

    fixed various things and such

--- parse.py
+++ parse.py
@@ -28,15 +28,17 @@ def parsate(filename):
                 return ln
             if type(ln) == dict:
                 return varlist[ln]
+        if type(ln) == str:
+            return ln
         if ln[1] == "fnop":
             endindex = 1
             while not ln[endindex] == "fnend":
                 endindex += 1
             return [ln[0], parse(ln[2:endindex])]
         if ln[1] in intops:
-            return ["intop", ln[0], parse(ln[2:]), intops[ln[1]]]
-        if ln[1] == "gop" or ln[1] == "lop":
-            return [boolops[ln[1]], ln[0], ln[2]]
+            return ["intop", parse(ln[0]), parse(ln[2:]), intops[ln[1]]]
+        if ln[1] in boolops:
+            return [boolops[ln[1]], parse(ln[0]), parse(ln[2:])]
         if ln[3] == "asop":
             return [ln[3], ln[2], parse(ln[4:])]
         return z
