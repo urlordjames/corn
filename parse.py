9335514e60c722132da34212870a74a4ccf35124
urlordjames <urlordjames@gmail.com>
2020-02-14 22:32:46 -0500

    somewhat improved debugging and fixed some things with jumps

--- parse.py
+++ parse.py
@@ -1,7 +1,9 @@
 from lex import lexate
 
-def parsate(filename):
+def parsate(filename, debug):
     tklist = lexate(filename)
+    if debug:
+        print(tklist)
 
     intops = {
         "+": "+",
@@ -18,6 +20,8 @@ def parsate(filename):
     }
     
     def parse(ln):
+        if debug:
+            print(ln)
         z = []
         if type(ln) == bool:
             return ln
@@ -34,7 +38,7 @@ def parsate(filename):
                 endindex += 1
             return [ln[0], parse(ln[2:endindex])]
         if ln[1] == "sepop":
-            return [ln[0], ln[2]]
+            return [ln[0], ln[2:]]
         if ln[1] in intops:
             return ["intop", parse(ln[0]), parse(ln[2:]), intops[ln[1]]]
         if ln[1] in boolops:
